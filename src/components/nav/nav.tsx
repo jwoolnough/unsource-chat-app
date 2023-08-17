026aba9172ac7b07bb46454c5135cdbbbcb552af
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2023-08-17 09:43:59 +0100

    Hook up log out

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,6 +1,7 @@
+import { signOut } from "firebase/auth";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { ComponentProps, cloneElement } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { ComponentProps, MouseEventHandler, cloneElement } from "react";
 import {
   FiHelpCircle,
   FiLogOut,
@@ -9,41 +10,62 @@ import {
   FiUser,
 } from "react-icons/fi";
 
+import { auth } from "@/services/firebase";
+
 import { clsxm } from "@/utils/clsxm";
 
 import { Tippy } from "../tippy";
 import "./style.css";
+import { toast } from "react-toastify";
 
-interface NavItemProps extends ComponentProps<"li"> {
+interface NavItemProps extends Omit<ComponentProps<"li">, "onClick"> {
   title: string;
-  href: ComponentProps<typeof Link>["href"];
+  href?: ComponentProps<typeof Link>["href"];
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   icon: React.ReactElement;
 }
 
-const NavItem = ({ title, href, icon, ...rest }: NavItemProps) => {
+const NavItem = ({ title, href, icon, onClick, ...rest }: NavItemProps) => {
   const isActive = href === usePathname();
+
+  const styledIcon = cloneElement(icon, {
+    size: 22,
+  });
+  const buttonClasses = clsxm(
+    "w-9 h-9 text-slate-400 flex items-center justify-center rounded-md",
+    "hover:text-orange-500",
+    isActive && "text-orange-500 bg-orange-100"
+  );
 
   return (
     <li {...rest}>
       <Tippy content={title}>
-        <Link
-          href={href}
-          className={clsxm(
-            "w-9 h-9 text-slate-400 flex items-center justify-center rounded-md",
-            "hover:text-orange-500",
-            isActive && "text-orange-500 bg-orange-100"
-          )}
-        >
-          {cloneElement(icon, {
-            size: 22,
-          })}
-        </Link>
+        {href ? (
+          <Link href={href} className={buttonClasses}>
+            {styledIcon}
+          </Link>
+        ) : (
+          <button type="button" onClick={onClick} className={buttonClasses}>
+            {styledIcon}
+          </button>
+        )}
       </Tippy>
     </li>
   );
 };
 
 const Nav = () => {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+      router.push("/login");
+    } catch {
+      toast.error("Unable to log out")
+    }
+  };
+
   return (
     <nav className="my-6">
       <ul className="flex flex-col h-full gap-[0.375rem]">
@@ -54,7 +76,7 @@ const Nav = () => {
         <NavItem
           title="Log out"
           icon={<FiLogOut />}
-          href="/logout"
+          onClick={handleLogOut}
           className="mt-auto"
         />
       </ul>
