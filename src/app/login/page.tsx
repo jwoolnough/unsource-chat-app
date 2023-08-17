3e2e3066cc1318411161827c8fa16673430fe753
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2023-08-17 09:43:59 +0100

    Add react hook form and setup validation states

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,32 @@
 "use client";
 import { Input, PasswordInput } from "@/components/input";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+  password: z.string().min(1, "Password is a required field"),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit: SubmitHandler<LoginSchema> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1 className="text-2xl font-bold text-center mb-4">Log in</h1>
 
       <Input
@@ -13,6 +35,8 @@ export default function Login() {
         placeholder="joe@bloggs.com"
         autoFocus
         autoComplete="username"
+        error={errors.email?.message}
+        {...register("email")}
       />
 
       <PasswordInput
@@ -21,6 +45,8 @@ export default function Login() {
         placeholder="••••••••••••"
         autoFocus
         autoComplete="current-password"
+        error={errors.password?.message}
+        {...register("password")}
       />
 
       <button type="submit" className="button button-full mb-4">
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -11,6 +11,7 @@ interface InputProps extends React.ComponentProps<"input"> {
   label: string;
   labelClassName?: string;
   renderRightElement?: RenderInputRightElement;
+  error?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -23,6 +24,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       // Slight hack to leverage `:placeholder-shown` trick within `style.css`
       placeholder = " ",
       renderRightElement,
+      error,
       ...rest
     },
     ref
@@ -30,39 +32,47 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     const id = useId();
 
     return (
-      <div
-        className={clsxm(
-          "input-container",
-          "relative mb-4",
-          containerClassName
-        )}
-      >
-        <label
+      <div className="mb-4">
+        <div
           className={clsxm(
-            "input-label",
-            "absolute top-[13px] left-[13px] inline-block origin-top-left cursor-text px-1 text-slate-200",
-            labelClassName
+            "input-container",
+            "relative",
+            error && "is-invalid",
+            containerClassName
           )}
-          htmlFor={id}
         >
-          {label}
-        </label>
-        <input
-          ref={ref}
-          id={id}
-          className={clsxm(
-            "input",
-            "w-full rounded-md border border-accent bg-transparent px-4 py-3",
-            "outline-none focus:border-orange-400",
-            "placeholder:text-slate-200 placeholder:text-opacity-75 placeholder:opacity-0 placeholder:transition-opacity",
-            className
-          )}
-          placeholder={placeholder}
-          {...rest}
-        />
-        {renderRightElement?.({
-          defaultClasses: "absolute right-[17px] top-1/2 -translate-y-1/2",
-        })}
+          <label
+            className={clsxm(
+              "input-label",
+              "absolute top-[13px] left-[13px] inline-block origin-top-left cursor-text px-1 text-slate-200",
+              labelClassName
+            )}
+            htmlFor={id}
+          >
+            {label}
+          </label>
+
+          <input
+            ref={ref}
+            id={id}
+            className={clsxm(
+              "input",
+              "w-full rounded-md border border-accent bg-transparent px-4 py-3",
+              "outline-none focus:border-orange-400",
+              "placeholder:text-slate-200 placeholder:text-opacity-75 placeholder:opacity-0 placeholder:transition-opacity",
+              error &&
+                "border-red-300 focus:border-red-300 focus:ring-2 ring-red-200",
+              className
+            )}
+            placeholder={placeholder}
+            {...rest}
+          />
+          {renderRightElement?.({
+            defaultClasses: "absolute right-[17px] top-1/2 -translate-y-1/2",
+          })}
+        </div>
+
+        {error && <p className="text-sm text-red-400 mt-1">{error}</p>}
       </div>
     );
   }
