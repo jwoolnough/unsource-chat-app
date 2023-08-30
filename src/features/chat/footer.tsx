797b4ae4c36ff174ec86fd0683e66c8d8b2138f3
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2023-08-30 20:57:25 +0100

    Submit on enter

--- src/features/chat/footer.tsx
+++ src/features/chat/footer.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { addDoc, collection } from "firebase/firestore";
-import { FormEventHandler, useState } from "react";
+import { useState } from "react";
 import { FiSend } from "react-icons/fi";
 import TextareaAutosize from "react-textarea-autosize";
+import { toast } from "react-toastify";
 
 import { auth } from "@/services/firebase";
 import { firestore } from "@/services/firebase/store";
@@ -16,26 +17,29 @@ const ChatFooter = () => {
   const [message, setMessage] = useState("");
   const { isAtBottom } = useLayoutStore();
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
+    try {
+      const user = auth.currentUser;
 
-    const user = auth.currentUser;
+      if (!user) {
+        throw new Error("Unauthenticated user");
+      }
 
-    if (!user) {
-      // TODO:
-      return;
+      const { uid, photoURL, displayName } = user;
+      await addDoc(collection(firestore, "messages"), {
+        content: message,
+        authorId: uid,
+        photoUrl: photoURL,
+        createdAt: new Date().toISOString(),
+        displayName,
+      });
+
+      setMessage("");
+    } catch (e) {
+      toast.error(
+        "There was an issue sending your message, please try again or contact support"
+      );
     }
-
-    const { uid, photoURL, displayName } = user;
-    await addDoc(collection(firestore, "messages"), {
-      content: message,
-      authorId: uid,
-      photoUrl: photoURL,
-      createdAt: new Date().toISOString(),
-      displayName,
-    });
-
-    setMessage("");
   };
 
   return (
@@ -44,20 +48,30 @@ const ChatFooter = () => {
         "sticky bottom-0 border-t border-slate-100 border-opacity-0 p-4 transition",
         !isAtBottom && "border-opacity-100 bg-slate-50/70 backdrop-blur"
       )}
-      onSubmit={handleSubmit}
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
     >
       <label className="sr-only">Message</label>
-      <div className="flex items-end gap-4 rounded-xl bg-white shadow-sm">
+      <div className="flex items-end rounded-xl bg-white shadow-sm">
         <TextareaAutosize
           className="block w-full resize-none rounded-bl-xl rounded-tl-xl px-4 py-3 outline-none placeholder:text-slate-200"
           placeholder="Write your message..."
           rows={1}
+          maxRows={5}
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-        ></TextareaAutosize>
+          onKeyDown={(e) => {
+            if (!e.shiftKey && e.code === "Enter") {
+              e.preventDefault();
+              handleSubmit();
+            }
+          }}
+        />
         <button
           type="submit"
-          className="button mb-[0.375rem] mr-[0.375rem] flex h-9 w-9 shrink-0 items-center justify-center rounded-md p-0"
+          className="button mb-1.5 mr-1.5 flex h-9 w-9 shrink-0 items-center justify-center rounded-md p-0"
           aria-label="Send"
           disabled={!message}
         >
